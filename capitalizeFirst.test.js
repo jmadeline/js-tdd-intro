144dcd15402e531ec20aa2c33f2a5a9f4a8dd9f5
Madeline Joubert <madelinejoubert44@gmail.com>
2020-01-08 16:38:50 +0100

    function capitalizeFirstLetters

--- capitalizeFirst.test.js
+++ capitalizeFirst.test.js
@@ -1,11 +1,22 @@
 const assert = require('assert');
-const capitalizeFirstLetters = ('./capitalizeFirstLetters');
+
+function capitalizeFirstLetters(text) {
+  if (text.length > 0) {
+    let wordsArray = text.split(' ')
+    let capsArray = wordsArray.map(word => {
+      return word[0].toUpperCase() + word.slice(1)
+    })
+    return capsArray.join(' ')
+  } else {
+    return '';
+  }
+}
 
 // Check that capitalizeFirstLetters is a function
 assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 // Check that capitalizeFirstLetters transforms i am learning TDD correctly
-assert.strictEqual(capitalizeFirst('i am learning TDD'), 'I Am Learning TDD');
+assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
 // Check that it works for a 1-character string
-assert.strictEqual(capitalizeFirst('z'), 'Z');
+assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
 // Check that it works for an empty string
-assert.strictEqual(capitalizeFirst(''), '');
+assert.strictEqual(capitalizeFirstLetters(''), '');
